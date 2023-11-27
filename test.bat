057d3bfe9df3441f73b75d775d7a96ea2461f397
Alfie Wang <awang@parasoft.com>
2023-11-27 16:33:13 +0800

    Add test reports.

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
-mvn jtest:jtest "-Djtest.settings=./localsettings.properties" "-Djtest.config=dtp://DTP Recommended Rules" "-Djtest.report=./build/report/jtest/static"
+mvn clean jtest:jtest "-Djtest.settings=./localsettings.properties" "-Djtest.config=dtp://DTP Recommended Rules" "-Djtest.report=./build/report/jtest/static"
 
-mvn process-test-classes jtest:agent test jtest:jtest "-Djtest.settings=./localsettings.properties" "-Djtest.config=builtin://Unit Tests" "-Djtest.report=./build/report/jtest/junit"
+mvn clean process-test-classes jtest:agent test jtest:jtest "-Djtest.settings=./localsettings.properties" "-Djtest.config=builtin://Unit Tests" "-Djtest.report=./build/report/jtest/junit"
